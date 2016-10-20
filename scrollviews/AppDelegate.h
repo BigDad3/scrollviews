06cc4054ccf6bd100b50fce88ab65ec93714f6fa
gaowei <gaowei@gaoweidemacbook-pro.local>
2016-10-20 10:27:04 +0800

    Initial Commit

---
+++ scrollviews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  scrollviews
+//
+//  Created by gaowei on 2016/10/20.
+//  Copyright © 2016年 xes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ scrollviews/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  scrollviews
+//
+//  Created by gaowei on 2016/10/20.
+//  Copyright © 2016年 xes. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
